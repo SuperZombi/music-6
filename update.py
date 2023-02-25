71d588529cc3761a598207aed60a45e479cce80d
Super Zombi <rost201541@gmail.com>
2023-02-25 20:32:05 +0200

    Create update.py

---
+++ update.py
@@ -0,0 +1,17 @@
+import os
+import shutil
+server_current = ""
+server_updates = "music-5"
+
+for dname, dirs, files in os.walk(server_updates):
+	for fname in files:
+		fpath = os.path.join(dname, fname)
+
+		rel = os.path.relpath(fpath, server_updates)
+		new = os.path.join(server_current, rel)
+		
+		if os.path.dirname(new) and not os.path.exists(os.path.dirname(new)):
+			os.mkdir(os.path.dirname(new))
+		shutil.move(fpath, new)
+
+shutil.rmtree(server_updates)
